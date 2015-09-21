179413ede89c90544a251b3975c07c5d4c49746a
Ben Bettridge <bbettridge@switch.tv>
2015-09-21 15:09:07 +1000

    Added timeout to HttpCheck class

--- checks/http_check.rb
+++ checks/http_check.rb
@@ -1,25 +1,34 @@
 require_relative('base_check')
+require('benchmark')
 class HttpCheck < BaseCheck
 
   def execute(params={})
     validate_params(params)
     client = HTTPClient.new
-    response = client.get(params['url'])
-    if response.code.to_s == params['response']
+    response = nil
+    response_time = Benchmark.measure do
+      response = client.get(params['url'])
+    end
+    if response_time.real > ((params['timeout'].to_i * 1.0) / 1000)
+      @failed = true
+      @output = "Exceeded timeout value of #{params['timeout']} milliseconds. Time taken: #{(response_time.real * 1000).round(2)} milliseconds"
+    elsif response.code.to_s == params['response']
       @failed = false
+      @output = "#{params['url']} responded with #{response.code.to_s} in #{(response_time.real * 1000).round(2)} milliseconds"
     else
       @failed = true
+      @output = "Response code #{response.code.to_s} does not match required value of #{params['response']}"
     end
-    @output = response.body
   end
 
   def help
     'HttpCheck requires the following params:
       url = <URL>
       response = <CODE>
+      timeout = <MILLISECONDS>
 
     Example:
-      HttpCheck url|http://www.google.com response|200'
+      HttpCheck url|http://www.google.com response|200 timeout|100'
   end
 
   def validate_params(params)
